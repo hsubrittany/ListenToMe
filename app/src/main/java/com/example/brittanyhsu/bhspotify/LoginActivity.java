fb6172a477b00aaae361b2591516e786d78b632d
hsubrittany <hsubrittany@gmail.com>
2017-08-01 23:18:26 -0700

    Added login activity; main activity now nearly empty

---
+++ app/src/main/java/com/example/brittanyhsu/bhspotify/LoginActivity.java
@@ -0,0 +1,146 @@
+package com.example.brittanyhsu.bhspotify;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class LoginActivity extends AppCompatActivity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
+    private final String CLIENT_ID = Constants.CLIENT_ID;
+    private final String REDIRECT_URI = Constants.REDIRECT_URI;
+    private String accessToken;
+    private static final int REQUEST_CODE = 12;
+    private Player mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fingerprint);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming", "playlist-read-private",
+                "playlist-modify-public", "playlist-modify-private"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                accessToken = response.getAccessToken();
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(LoginActivity.this);
+                        mPlayer.addNotificationCallback(LoginActivity.this);
+                        Log.d("LoginActivity", "onInitialized");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("LoginActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+            else
+                Log.d("LoginActivity", "AuthenticationResponse.Type.TOKEN messed up :-(");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        android.support.v7.widget.SearchView searchView = (android.support.v7.widget.SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("LoginActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("LoginActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("LoginActivity", "User logged in");
+//        mPlayer.playUri(null, "spotify:track:4TJNW3JPNoxtsqmZjLKGk0", 0, 0);
+
+//        Intent intent = new Intent(LoginActivity.this, GetMyPlaylists.class);
+//        Intent intent = new Intent(LoginActivity.this, SearchableActivity.class);
+//        Intent intent = new Intent(LoginActivity.this, AddToPlaylist.class);
+        Intent intent = new Intent(LoginActivity.this, FingerprintActivity.class);
+        intent.putExtra("access token", accessToken);
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("LoginActivity", "User logged out");
+    }
+
+
+
+    @Override
+    public void onLoginFailed(Error error) {
+        Log.d("LoginActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("LoginActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("LoginActivity", "Received connection message: " + message);
+    }
+}
