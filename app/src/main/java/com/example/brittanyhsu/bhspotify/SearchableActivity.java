bf891dcc8f814d1501b31ef5cb2225cb435fef28
hsubrittany <hsubrittany@gmail.com>
2017-08-05 00:05:42 -0700

    Fixes error from when trying to hide keyboard but keyboard wasn't there

--- app/src/main/java/com/example/brittanyhsu/bhspotify/SearchableActivity.java
+++ app/src/main/java/com/example/brittanyhsu/bhspotify/SearchableActivity.java
@@ -99,7 +99,8 @@ public class SearchableActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        if(getCurrentFocus() != null)
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
         return true;
     }
 
